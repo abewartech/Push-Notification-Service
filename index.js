dd1a28a70438f03ee7e43f0ba518d42321074730
ABE <abe@pernika>
2022-06-01 09:08:03 +0700

    AB : MQTT

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const HOST = "0.0.0.0";
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://localhost");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+  client.publish("sendnotif", "Hello mqtt");
+});
+
+client.on("connect", function () {
+  client.subscribe("sendnotif", function (err) {
+    if (!err) {
+      console.log(err);
+    }
+  });
+});
+
+client.on("message", function (topic, message) {
+  if (topic === "sendnotif") {
+    console.log(message.toString());
+  }
+  client.end();
 });
 
 app.listen(PORT, HOST);
