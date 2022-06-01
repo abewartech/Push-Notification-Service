1dc2a6951757d99e64a8e0f40a507efc3c712109
ABE <abe@pernika>
2022-06-01 08:47:23 +0700

    AB : Express + Docker

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, HOST);
+console.log(`Our app running on http://${HOST}:${PORT}`);
